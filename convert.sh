45f9b0f6e8e768f498efc6bdc207f48fd8f82a5c
zianpan <92709417+zianpan@users.noreply.github.com>
2025-03-10 12:23:25 -0400

    run no rag

--- convert.sh
+++ convert.sh
@@ -6,11 +6,16 @@ python scripts/utils/csv_to_json.py \
     --reference_dir test/references/
 
 python scripts/utils/csv_to_json.py \
-    --csv_file results/test_qa_pairs_new_100_2.csv \
+    --csv_file results/test_qa_pairs_new_400.csv \
     --output_dir test/outputs/ \
     --reference_dir test/references/
 
 python scripts/utils/csv_to_json.py \
-    --csv_file results/test_qa_pairs_new_600_38.csv \
+    --csv_file results/test_qa_pairs_400_no_rag.csv \
+    --output_dir test/outputs/ \
+    --reference_dir test/references/
+
+python scripts/utils/csv_to_json.py \
+    --csv_file results/test_qa_pairs_new_2500_38.csv \
     --output_dir test/outputs/ \
     --reference_dir test/references/
