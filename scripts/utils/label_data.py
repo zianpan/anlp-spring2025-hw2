a5fc2a9afcc6b2aa297a25e20806c795c739455e
niconicoo0w0 <zhilanw2@illinois.edu>
2025-03-11 12:12:38 -0400

    add labeled results

--- scripts/utils/label_data.py
+++ scripts/utils/label_data.py
@@ -3,6 +3,9 @@ from transformers import pipeline
 import torch
 import os
 
+os.chdir("..")
+os.chdir("..")
+
 device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
 
 print(f"Using device: {device}")
